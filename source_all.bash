c2fb98597dd1e74ced9d8cad0fabec67e02e32bb
Oscar <oscardawe@googlemail.com>
2022-04-16 18:01:45 +0100

    edited launch files

--- source_all.bash
+++ source_all.bash
@@ -3,7 +3,9 @@
 PIONEER3DX_DRIVERS_WS=~/pioneer3dx_wss/pioneer3dx
 PIONEER3DX_SIMULATION_WS=~/pioneer3dx_wss/pioneer3dx_simulation
 PIONEER3DX_2DSLAM_WS=~/pioneer3dx_wss/pioneer3dx_2dslam
+PIONEER3DX_map_and_nav_WS=~/pioneer3dx_wss/pioneer3dx_map_and_nav
 
 source $PIONEER3DX_DRIVERS_WS/devel/setup.bash --extend
 source $PIONEER3DX_SIMULATION_WS/devel/setup.bash --extend
-source $PIONEER3DX_2DSLAM_WS/devel/setup.bash –extend
+source $PIONEER3DX_2DSLAM_WS/devel/setup.bash --extend
+source $PIONEER3DX_map_and_nav_WS/devel/setup.bash --extend
