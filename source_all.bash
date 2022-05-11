d24b69fed7d9fb8bf874a1f399212cf0cc9be26e
Steve Knight <stephen.d.knight@students.plymouth.ac.uk>
2022-05-11 17:00:59 +0100

    basically just spelling mistakes
    sourcing all the stuff from the readme in the source all

--- source_all.bash
+++ source_all.bash
@@ -4,10 +4,14 @@ PIONEER3DX_DRIVERS_WS=~/pioneer3dx_wss/pioneer3dx
 PIONEER3DX_SIMULATION_WS=~/pioneer3dx_wss/pioneer3dx_simulation
 PIONEER3DX_2DSLAM_WS=~/pioneer3dx_wss/pioneer3dx_2dslam
 PIONEER3DX_map_and_nav_WS=~/pioneer3dx_wss/pioneer3dx_map_and_nav
+PIONEER3DX_sensorfusion_WS=~/pioneer3dx_wss/pioneer3dx_sensorfusion
 
 source ~/.bashrc
 source /opt/ros/noetic/setup.bash
+
 source $PIONEER3DX_DRIVERS_WS/devel/setup.bash --extend
 source $PIONEER3DX_SIMULATION_WS/devel/setup.bash --extend
 source $PIONEER3DX_2DSLAM_WS/devel/setup.bash --extend
 source $PIONEER3DX_map_and_nav_WS/devel/setup.bash --extend
+source $PIONEER3DX_sensorfusion_WS/devel/setup.bash --extend
+
