29dd72d2d6ae59fcc8e22d094a5c61193e8b241d
Steve Knight <stephen.d.knight@students.plymouth.ac.uk>
2022-05-11 16:16:52 +0100

    basically just a test
    added an all file to the sim launchers, and added the environment source to the source all .bash

--- source_all.bash
+++ source_all.bash
@@ -5,6 +5,8 @@ PIONEER3DX_SIMULATION_WS=~/pioneer3dx_wss/pioneer3dx_simulation
 PIONEER3DX_2DSLAM_WS=~/pioneer3dx_wss/pioneer3dx_2dslam
 PIONEER3DX_map_and_nav_WS=~/pioneer3dx_wss/pioneer3dx_map_and_nav
 
+source ~/.bashrc
+source /opt/ros/noetic/setup.bash
 source $PIONEER3DX_DRIVERS_WS/devel/setup.bash --extend
 source $PIONEER3DX_SIMULATION_WS/devel/setup.bash --extend
 source $PIONEER3DX_2DSLAM_WS/devel/setup.bash --extend
